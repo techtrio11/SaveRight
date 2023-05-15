7d8c3687a7d98472145b6513532b4eb782becf00
techtrio11 <techtrio11@gmail.com>
2023-05-15 12:09:22 -0600

    Added education screen

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 //Screens
+import Education from "./src/education";
 import Home from "./src/Home";
 import SmallGoal from "./src/small_goal";
 
 // Screen Names
 const homeName = "Home";
 const smallGoalName = "Small Goal";
+const educationName = "Education";
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -25,11 +27,14 @@ export default function App() {
               iconName = focused ? "home" : "home-outline";
             } else if (rn === smallGoalName) {
               iconName = focused ? "list" : "list-outline";
+            } else if (rn === educationName) {
+              iconName = focused ? "book" : "book-outline";
             }
             return <Ionicons name={iconName} />;
           },
         })}
       >
+        <Tab.Screen name={educationName} component={Education} />
         <Tab.Screen name={homeName} component={Home} />
         <Tab.Screen name={smallGoalName} component={SmallGoal} />
       </Tab.Navigator>
---
+++ src/education/index.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default function Home({ navigation }) {
+  return (
+    <View>
+      <Text> Learn how to save here! </Text>
+    </View>
+  );
+}
