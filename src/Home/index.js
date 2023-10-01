c58ba946887a798cd62ea2aff066d4513d26041e
techtrio11 <techtrio11@gmail.com>
2023-10-01 17:12:58 -0600

    Added logo and text to 'Home' screen, and built out skeleton for 'Education' page.

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,10 +1,44 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  Image,
+} from "react-native";
+import logo from "../../assets/SaveRight_Logo.png";
 
 export default function Home({ navigation }) {
   return (
-    <View>
-      <Text> Hello! </Text>
+    <View style={styles.container}>
+      <Image style={styles.image} source={logo} />
+      <Text style={styles.text}>
+        {" "}
+        Use the buttons below to help achieve your saving goals!
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: "68%",
+    height: "40%",
+  },
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "column",
+    position: "relative",
+    backgroundColor: "#ffffff",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- src/education/index.js
+++ src/education/index.js
@@ -1,10 +1,31 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  Image,
+} from "react-native";
 
 export default function Home({ navigation }) {
   return (
     <View>
-      <Text> Learn how to save here! </Text>
+      <Text style={styles.title}> Title 1 </Text>
+      <Text>Paragraph 1</Text>
+      <Text> </Text>
+      <Text style={styles.title}> Title 2 </Text>
+      <Text>Paragraph 2</Text>
+      <Text> </Text>
+      <Text style={styles.title}> Title 3 </Text>
+      <Text>Paragraph 3</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
